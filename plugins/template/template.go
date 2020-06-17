88b12a059db1a4b0f18119e227accb5b01f7c1d9
Saman Hosseini <bounoable@gmail.com>
2020-06-17 03:18:44 +0200

    feat(template): work on plugin

--- plugins/template/template.go
+++ plugins/template/template.go
@@ -2,20 +2,92 @@
 package template
 
 import (
+	"context"
+	"html/template"
+	"strings"
+
+	"github.com/bounoable/postdog/letter"
 	"github.com/bounoable/postdog/office"
 )
 
 type plugin struct {
-	cfg Config
+	cfg  Config
+	tpls *template.Template
 }
 
-// Plugin ...
+// Plugin creates the template plugin.
+// It panics if it fails to parse the templates.
+// Use TryPlugin() if you need to catch parse errors.
+//
+// Example:
+//	plugin := template.Plugin(
+//		template.UseDir("/templates")
+//	)
 func Plugin(opts ...Option) office.Plugin {
-	return plugin{
-		cfg: newConfig(opts...),
+	plugin, err := TryPlugin(opts...)
+	if err != nil {
+		panic(err)
 	}
+	return plugin
 }
 
-func (p plugin) Install(ctx office.PluginContext) {
-
+// TryPlugin creates the template plugin. It doesn't panic then it fails to parse the templates.
+func TryPlugin(opts ...Option) (office.Plugin, error) {
+	cfg := newConfig(opts...)
+	tpls, err := cfg.ParseTemplates()
+	if err != nil {
+		return nil, err
+	}
+	return plugin{
+		cfg:  cfg,
+		tpls: tpls,
+	}, nil
 }
+
+func (p plugin) Install(pctx office.PluginContext) {
+	pctx.WithMiddleware(
+		office.MiddlewareFunc(func(
+			ctx context.Context,
+			let letter.Letter,
+			next func(context.Context, letter.Letter) (letter.Letter, error),
+		) (letter.Letter, error) {
+			name, ok := Name(ctx)
+			if !ok {
+				return next(ctx, let)
+			}
+
+			var builder strings.Builder
+
+			if err := p.tpls.ExecuteTemplate(&builder, name, struct{ Letter letter.Letter }{
+				Letter: let,
+			}); err != nil {
+				return let, err
+			}
+
+			let.HTML = builder.String()
+
+			return next(ctx, let)
+		}),
+	)
+}
+
+// Enable sets the template that will be used to build the letter body for this context.
+func Enable(ctx context.Context, name string) context.Context {
+	return context.WithValue(ctx, ctxTemplate, name)
+}
+
+// Disable disables templates for this context.
+func Disable(ctx context.Context) context.Context {
+	return context.WithValue(ctx, ctxTemplate, nil)
+}
+
+// Name returns the template that should be used for sending letter with ctx.
+// Returns false if ctx has no template set.
+func Name(ctx context.Context) (string, bool) {
+	name, ok := ctx.Value(ctxTemplate).(string)
+	return name, ok
+}
+
+type ctxKey string
+
+var ctxTemplate = ctxKey("template")
--- plugins/template/template_test.go
+++ plugins/template/template_test.go
@@ -1,1 +1,88 @@
 package template_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/bounoable/postdog/letter"
+	"github.com/bounoable/postdog/office"
+	"github.com/bounoable/postdog/office/mock_office"
+	"github.com/bounoable/postdog/plugins/template"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEnable(t *testing.T) {
+	ctx := context.Background()
+
+	name, ok := template.Name(ctx)
+	assert.Empty(t, name)
+	assert.False(t, ok)
+
+	ctx = template.Enable(ctx, "test")
+	name, ok = template.Name(ctx)
+	assert.Equal(t, "test", name)
+	assert.True(t, ok)
+
+	ctx = template.Disable(ctx)
+	name, ok = template.Name(ctx)
+	assert.Empty(t, name)
+	assert.False(t, ok)
+}
+
+func TestPlugin(t *testing.T) {
+	wd, _ := os.Getwd()
+
+	plugin := template.Plugin(
+		template.Use("test1", filepath.Join(wd, "testdata", "templates", "tpl1.html")),
+		template.Use("test2", filepath.Join(wd, "testdata", "templates", "tpl2.html")),
+		template.UseDir(filepath.Join(wd, "testdata", "templateDirs", "dir1")),
+		template.UseDir(filepath.Join(wd, "testdata", "templateDirs", "dir2")),
+	)
+
+	names := []string{
+		"test1",
+		"test2",
+		"tpl3",
+		"tpl4",
+		"tpl5",
+		"tpl6",
+		"nested.tpl7",
+	}
+
+	for i, name := range names {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			ctx := context.Background()
+			ctx = template.Enable(ctx, name)
+
+			off := office.New(office.WithPlugin(plugin))
+
+			let := letter.Write(letter.Text("example body"))
+			expectedLet := makeExpectedLetter(let, i+1)
+
+			trans := mock_office.NewMockTransport(ctrl)
+			trans.EXPECT().Send(ctx, expectedLet)
+
+			off.ConfigureTransport("test", trans)
+
+			err := off.Send(ctx, let)
+			assert.Nil(t, err)
+		})
+	}
+}
+
+func makeExpectedLetter(let letter.Letter, num int) letter.Letter {
+	let.HTML = fmt.Sprintf(`<h1>Template %d</h1>
+
+<p>
+  example body
+</p>
+`, num)
+	return let
+}
