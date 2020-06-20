3e45ebd99c7d89ac909c2797f64e564463bc131f
Saman Hosseini <bounoable@gmail.com>
2020-06-20 20:17:33 +0200

    feat(autowire): add no-op transport `Register` function

--- transport/nop/nop.go
+++ transport/nop/nop.go
@@ -3,6 +3,8 @@ package nop
 import (
 	"context"
 
+	"github.com/bounoable/postdog"
+	"github.com/bounoable/postdog/autowire"
 	"github.com/bounoable/postdog/letter"
 )
 
@@ -13,6 +15,15 @@ var (
 	// Transport is the no-op transport.
 	Transport = transport{}
 )
+
+// Register registers the no-op transpot in the autowire config.
+func Register(cfg *autowire.Config) {
+	cfg.RegisterProvider(Provider, autowire.TransportFactoryFunc(
+		func(_ context.Context, _ map[string]interface{}) (postdog.Transport, error) {
+			return Transport, nil
+		}),
+	)
+}
 
 type transport struct{}
 
