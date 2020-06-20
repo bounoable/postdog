d43517b279021bc382b707a41f9523ccd9497e35
Saman Hosseini <bounoable@gmail.com>
2020-06-20 18:02:54 +0200

    docs(markdown): add autowire example [ci skip]

--- plugin/markdown/example_test.go
+++ plugin/markdown/example_test.go
@@ -2,8 +2,10 @@ package markdown_test
 
 import (
 	"context"
+	"strings"
 
 	"github.com/bounoable/postdog"
+	"github.com/bounoable/postdog/autowire"
 	"github.com/bounoable/postdog/letter"
 	"github.com/bounoable/postdog/plugin/markdown"
 	gm "github.com/bounoable/postdog/plugin/markdown/goldmark"
@@ -25,6 +27,28 @@ func Example_basic() {
 	))
 
 	_ = err
+}
+
+func Example_autowire() {
+	config := `
+plugins:
+  - name: markdown
+	use: goldmark
+`
+
+	cfg, err := autowire.Load(strings.NewReader(config))
+	if err != nil {
+		panic(err)
+	}
+
+	po, err := cfg.Office(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	err = po.Send(context.Background(), letter.Write(
+		letter.Text("# Heading 1\n# Heading 2"),
+	))
 }
 
 func Example_disable() {
